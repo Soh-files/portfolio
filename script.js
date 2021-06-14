46a022a4a93c986135c567cdd91bcd84b296ca6c
そーたろう <81458517+Soh-files@users.noreply.github.com>
2021-06-14 10:49:24 +0900

    2021-06-14 adding animation.
    JavaScriptを使用してアニメーションを追加

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // document.getElementById('head-title').classList.add('show');
+    // ページが読み込まれたときに「KAura」を表示する
+    // const headTitle = document.getElementById('head-title');
+    // window.addEventListener('load', function () {
+    //     headTitle.classList.add('show');
+    // })
 
     // モバイル画面のメニュー開閉
     const $menuBtn = document.getElementById('menuBtn');
@@ -28,5 +32,56 @@ document.addEventListener('DOMContentLoaded', function() {
     }
     $topBtn.addEventListener('click', function () {
         timer = setInterval(setTimer, 10);
+    });
+
+    // コンテンツの見出しを1文字ずつアニメーションする
+    const animationTargetElements = document.querySelectorAll('.animationTitle');
+    const titleLine = document.querySelectorAll('.title-bg-anime');
+
+    for (let i = 0; i < animationTargetElements.length; i++) {
+        const targetElement = animationTargetElements[i];
+        const texts = targetElement.textContent;
+        const textsArray = [];
+
+        targetElement.textContent = '';
+
+        for (let j = 0; j < texts.split('').length; j++) {
+            const t = texts.split('')[j];
+            if (t === ' ') {
+                textsArray.push(' ');
+            } else {
+                textsArray.push('<span style="animation-delay: ' + ( j * 0.1 + 1 ) + 's;">' + t + '</span>');
+            }
+        }
+        
+        for (let k = 0; k < textsArray.length; k++) {
+            targetElement.innerHTML += textsArray[k];
+        }
+    }
+
+    // 見出しのアニメーションをスクロールしたときに開始するようにする
+    document.addEventListener('scroll', function() {
+        for (let l = 0; l < animationTargetElements.length; l++) {
+            const getElementDistance = animationTargetElements[l].getBoundingClientRect().top + animationTargetElements[l].clientHeight * 0.6;
+            if(window.innerHeight > getElementDistance) {
+                animationTargetElements[l].classList.add('showAnime');
+            }
+        }
+        for (let n = 0; n < titleLine.length; n++) {
+            const getTitleDistance = titleLine[n].getBoundingClientRect().top + titleLine[n].clientHeight;
+            if(window.innerHeight > getTitleDistance) {
+                titleLine[n].classList.add('showAnime');
+            }
+        }
+    });
+
+    const scrollAnimeTarget = document.querySelectorAll('.scrollAnime');
+    document.addEventListener('scroll', function () {
+        for(let m = 0; m < scrollAnimeTarget.length; m++) {
+            const scrollTargetDistance = scrollAnimeTarget[m].getBoundingClientRect().top + scrollAnimeTarget[m].clientHeight * 0.8;
+            if(window.innerHeight > scrollTargetDistance) {
+                scrollAnimeTarget[m].classList.add('showContent');
+            }
+        } 
     });
 });
